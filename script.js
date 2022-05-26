dc7a6c80ca9ed82133e9d78db7b719c035f0920c
Jorge Mendez <mendezjorge98@gmail.com>
2022-05-26 02:15:04 -0300

    added header responsive design

--- script.js
+++ script.js
@@ -3,20 +3,30 @@ const mainSection = document.querySelector('#main');
 const aboutUsSection = document.querySelector('#aboutUs');
 const contactSection = document.querySelector('#contact')
 
+const navLinks = document.querySelector('.nav__categorias');
 const navServices = document.querySelector('#navServices');
 const navAboutUs = document.querySelector('#navAboutUs');
 const navContact = document.querySelector('#navContact');
 
+const responsiveBtnNav = document.querySelector('.nav__responsiveButton');
+
 navServices.addEventListener('click',(e)=>{
     e.preventDefault();
+    if (navLinks.classList.contains('showNav')) navLinks.classList.toggle('showNav');
     scrollTo(0,servicesSection.offsetTop - 80);
 });
 
 navAboutUs.addEventListener('click',(e)=>{
     e.preventDefault();
+    if (navLinks.classList.contains('showNav')) navLinks.classList.toggle('showNav');
     scrollTo(0,aboutUsSection.offsetTop - 80);
 });
 navContact.addEventListener('click',(e)=>{
     e.preventDefault();
+    if (navLinks.classList.contains('showNav')) navLinks.classList.toggle('showNav');
     scrollTo(0,contactSection.offsetTop - 80);
 });
+
+responsiveBtnNav.addEventListener('click', ()=>{    
+    navLinks.classList.toggle('showNav');
+})
